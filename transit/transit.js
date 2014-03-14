169b944949ed963fd0a463d148a178c4c7618d32
Seth Kahn <Seth@resnet161-188.medford.tufts.edu>
2014-03-14 15:36:40 -0400

    console logging trains array

--- transit/transit.js
+++ transit/transit.js
@@ -240,6 +240,7 @@ function getInfo(name){
 	trains = trains.sort(function(a,b){
 		return a[1] - b[1];
 	});
+	console.log(trains);
 	for (j = 0; j < numTrains; j++){
 		html = html + trains[j][0];
 	}
